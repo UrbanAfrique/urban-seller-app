e95d570565bcf7c572ab2000b453e580a7436716
UrbanAfrique <urbanafrique2023@gmail.com>
2025-04-22 12:12:13 +0100

    Update SettingController.php

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -16,7 +16,7 @@ class SettingController extends Controller
         $routeType = RouteTypeEnum::SETTING;
         $pageTitle = 'Manage Settings';
         $setting = $seller->setting;
-        dd($setting);
+        dd($seller);
         return \view('app.settings.index', compact(
             'routeType',
             'setting',
