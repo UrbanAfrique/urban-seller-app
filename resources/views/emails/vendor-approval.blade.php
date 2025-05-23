9c985921296403e3a3cb802a25d9b81a16384c72
UrbanAfrique <urbanafrique2023@gmail.com>
2025-05-23 15:50:21 +0100

    test commit

--- resources/views/emails/vendor-approval.blade.php
+++ resources/views/emails/vendor-approval.blade.php
@@ -1,5 +1,5 @@
 @include('emails.components.mail-header',['mailHeading'=>$mailHeading])
-<main style="padding: 10px;text-align: center;">
+<main style="padding: 10px;text-align: center;"> 
     <p>{!! $mailDescription !!}</p>
     @if($type==\App\Enum\ApprovedStatusEnum::APPROVED)
         <a style="margin: 0 auto;background-color: #4374e0;padding: 10px;color: white;border-radius: 10px;text-decoration: none;"  target="_blank" href="{{ $loginUrl }}">
